cc142d2622ce967c8552907693f719918689f936
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2026-01-30 14:21:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+let submitBtn = document.getElementById("submit");
+let nameInput = document.getElementById("username");
+let passwordInput = document.getElementById("password");
+let checkbox = document.getElementById("checkbox");
+let existingBtn = document.getElementById("existing");
+let form = document.querySelector("form");
+
+
+const savedUsername = localStorage.getItem("username");
+const savedPassword = localStorage.getItem("password");
+
+if(savedPassword && savedUsername){
+	existingBtn.style.display = "inline-block";
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	
+	let name = nameInput.value;
+	let password = passwordInput.value;
+	
+	alert(`Logged in as ${name}`);
+
+	if(checkbox.checked){
+		localStorage.setItem("username", name);
+		localStorage.setItem("password", password);
+		existingBtn.style.display = "inline-block";
+	} else {
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");
+		existingBtn.style.display = "none";
+	}
+})
+
+existingBtn.addEventListener("click", () => {
+	const storedUsername = localStorage.getItem("username");
+	if(storedUsername){
+		alert(`Logged in as ${storedUsername}`);
+	}
+	
+	
+})
